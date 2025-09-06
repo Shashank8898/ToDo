1e12788ba516dc7cc0aae301fd1cf862ab1da4d1
Shashank <tiwarishashank950@gmail.com>
2025-09-06 15:29:28 +0530

    Setting up ENV and Appwrite

---
+++ client/src/conf/config.js
@@ -0,0 +1,11 @@
+const conf = {
+    appwriteUrl:String(import.meta.env.VITE_APPWRITEURL),
+    appwriteProjectId:String(import.meta.env.VITE_PROJECT_ID),
+    appwriteDatabaseId:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId:String(VITE_APPWRITE_BUCKET_ID),
+
+
+}
+
+export default conf;
